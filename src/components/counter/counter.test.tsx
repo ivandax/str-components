c51c663c937b49fc710de6316ffe973780055b65
ivandax <ivandax_89@hotmail.com>
2026-03-14 10:54:17 +0100

    Extend counter tests

--- src/components/counter/counter.test.tsx
+++ src/components/counter/counter.test.tsx
@@ -1,9 +1,30 @@
 import { render, screen } from "@testing-library/react";
 import { Counter } from "./counter";
+import userEvent from "@testing-library/user-event";
 
 describe("Counter", () => {
   it("renders initially with count of 0", () => {
     render(<Counter />);
+    expect(screen.getByText("0")).toBeInTheDocument();
+  });
+
+  it("increments count when + is clicked", async () => {
+    render(<Counter />);
+    await userEvent.click(screen.getByText("+"));
+    expect(screen.getByText("1")).toBeInTheDocument();
+  });
+
+  it("decrements count when - button is clicked", async () => {
+    render(<Counter />);
+    await userEvent.click(screen.getByText("+"));
+    await userEvent.click(screen.getByText("+"));
+    await userEvent.click(screen.getByText("-"));
+    expect(screen.getByText("1")).toBeInTheDocument();
+  });
+
+  it("decrements does not work if count = 0", async () => {
+    render(<Counter />);
+    await userEvent.click(screen.getByText("-"));
     expect(screen.getByText("0")).toBeInTheDocument();
   });
 });
