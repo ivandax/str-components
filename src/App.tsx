6c81c34faed2dc9c8c6023ce21a93ce50315ca45
ivandax <ivandax_89@hotmail.com>
2026-03-18 19:07:55 +0100

    Completed task

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import "./App.css";
 import { MyComponent } from "./components/my-component";
+import type { OnboardingForm } from "./components/my-component/my-component";
 
 function App() {
+  const dummyData: OnboardingForm = {
+    formTitle: "Business information",
+    fields: [
+      { name: "country", label: "Country", format: "country" },
+      { name: "businessName", label: "Business name", format: "string" },
+      { name: "businessUrl", label: "Business URL", format: "url" }
+    ],
+  };
+
   return (
     <div className="app">
-      <MyComponent />
+      <MyComponent formData={dummyData} />
     </div>
   );
 }
--- src/components/my-component/my-component.tsx
+++ src/components/my-component/my-component.tsx
@@ -1,5 +1,148 @@
+import { useState } from "react";
 import "./my-component.css";
 
-export function MyComponent() {
-  return <div className="myComponent">Hello world</div>;
+type OnboardingFormField = {
+  name: string;
+  label: string;
+  format: "country" | "string" | "url";
+};
+
+export type OnboardingForm = {
+  formTitle: string;
+  fields: OnboardingFormField[];
+};
+
+// send data as props to the component
+
+interface MyComponentProps {
+  formData: OnboardingForm;
+}
+
+export function MyComponent({ formData }: MyComponentProps) {
+  const { formTitle, fields } = formData;
+  // iterate over the array and create the form
+
+  const buildFormData = (): Record<string, string> => {
+    return fields.reduce((acc, field) => {
+      return {
+        ...acc,
+        [field.name]: "",
+      };
+    }, {});
+  };
+
+  // state management
+  const [formState, setFormState] =
+    useState<Record<string, string>>(buildFormData());
+
+  const [error, setError] = useState<null | string>(null);
+
+  const validateForm = (): boolean => {
+    let failsValidation = false;
+    const itemsToValidate = Object.keys(formState);
+    itemsToValidate.forEach((key) => {
+      const format = fields.find((field) => field.name === key)?.format;
+      if (format === "string") {
+        const value = formState[key];
+        if (value === "") {
+          failsValidation = true;
+        }
+      }
+      if (format === "url") {
+        const value = formState[key];
+        if (value === "" || !value.startsWith("http/https")) {
+          failsValidation = true;
+        }
+      }
+    });
+    return failsValidation;
+  };
+
+  console.log(formState);
+
+  return (
+    <div className="myComponent">
+      <form
+        className="formContent"
+        onSubmit={(e) => {
+          e.preventDefault();
+          setError(null);
+          console.log("attempts validations");
+
+          const failsValidation = validateForm();
+          if (failsValidation) {
+            setError("Missing field");
+          }
+          // do validation
+        }}
+      >
+        <h1>{formTitle}</h1>
+        {fields.map((field) => {
+          if (field.format === "string") {
+            return (
+              <label htmlFor={field.name}>
+                {field.label}
+                <input
+                  id={field.name}
+                  value={formState[field.name]}
+                  onChange={(e) => {
+                    setFormState((prev) => {
+                      return {
+                        ...prev,
+                        [field.name]: e.target.value,
+                      };
+                    });
+                  }}
+                />
+              </label>
+            );
+          }
+          if (field.format === "country") {
+            return (
+              <label htmlFor={field.name}>
+                {field.label}
+                <select
+                  name="country"
+                  id={field.name}
+                  onChange={(e) => {
+                    setFormState((prev) => {
+                      return {
+                        ...prev,
+                        [field.name]: e.target.value,
+                      };
+                    });
+                  }}
+                >
+                  <option value="">--Please choose an option--</option>
+                  <option value="US">US</option>
+                  <option value="Canada">Canada</option>
+                </select>
+              </label>
+            );
+          }
+          if (field.format === "url") {
+            return (
+              <label htmlFor={field.name}>
+                {field.label}
+                <input
+                  id={field.name}
+                  value={formState[field.name]}
+                  onChange={(e) => {
+                    setFormState((prev) => {
+                      return {
+                        ...prev,
+                        [field.name]: e.target.value,
+                      };
+                    });
+                  }}
+                />
+              </label>
+            );
+          }
+        })}
+        {error && <span>{error}</span>}
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
 }
